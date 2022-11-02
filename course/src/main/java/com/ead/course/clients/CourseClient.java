f89f6b6948d646f28f50a1fab1bb49b5023f9192
elberthbrandao <zelltr@hotmail.com>
2022-11-02 12:55:55 -0300

    implemented API Composition Pattern for course microservice

---
+++ course/src/main/java/com/ead/course/clients/CourseClient.java
@@ -0,0 +1,57 @@
+package com.ead.course.clients;
+
+import com.ead.course.dtos.ResponsePageDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class CourseClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    UtilsService utilsService;
+
+    @Value("${ead.api.url.authuser}")
+    String REQUEST_URI;
+
+    public Page<UserDto> getAllUsersByCourse(Pageable pageable, UUID courseId) {
+        List<UserDto> searchResult;
+        ResponseEntity<ResponsePageDto<UserDto>> result = null;
+
+        String url = REQUEST_URI + utilsService.createUrlGetAllUsersByCourse(pageable, courseId);
+
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+
+        try {
+            ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Response Number of Elements: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e){
+            log.error("Error request /courses {} ", e);
+        }
+
+        log.info("Ending request /users courseId {} ", courseId);
+
+        return result.getBody();
+    }
+
+}
